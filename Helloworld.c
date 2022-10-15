c3068b287108ebdbdc763778c4bd9a903ed19fd5
Craig McIlwrath <craigmc08@gmail.com>
2022-10-15 11:45:28 -0400

    change dandyhacks to world

--- Helloworld.c
+++ Helloworld.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, Dandyhacks! From CIF");
+   printf("Hello, World! From CIF");
    return 0;
 }
